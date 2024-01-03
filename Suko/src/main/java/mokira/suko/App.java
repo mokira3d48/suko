71ce69437732628698f5bef032288d3528491e3f
Dr Mokira <arnoldmokidra3d48@gmail.com>
2024-01-02 15:40:12 -0900

    OK.

--- Suko/src/main/java/mokira/suko/App.java
+++ Suko/src/main/java/mokira/suko/App.java
@@ -1,5 +1,6 @@
 package mokira.suko;
 
+import java.io.File;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,32 +8,37 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.scene.image.Image;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    private static Scene scene;
+  private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Super Kalculator");
+    primaryStage.setScene(new Scene(root, 435, 560));
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+    // String iconPath = getClass().getResource("../").getPath()
+    //        ;
+    // primaryStage.getIcons().add(new Image(new File(iconPath).getAbsolutePath()));
+    primaryStage.show();
+  }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+  static void setRoot(String fxml) throws IOException {
+    scene.setRoot(loadFXML(fxml));
+  }
 
-    public static void main(String[] args) {
-        launch();
-    }
+  private static Parent loadFXML(String fxml) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+    return fxmlLoader.load();
+  }
 
+  public static void main(String[] args) {
+    launch();
+  }
 }
